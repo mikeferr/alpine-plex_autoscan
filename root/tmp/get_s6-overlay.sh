69f19e3c7945429b265e14b36b4bd3b668f038e1
mikeferr <mikeferr82@gmail.com>
2022-02-22 07:12:38 -0500

    Update get_s6-overlay.sh

--- root/tmp/get_s6-overlay.sh
+++ root/tmp/get_s6-overlay.sh
@@ -4,7 +4,7 @@ if [ -n $ARCH ]
 then
   s6_arch=$ARCH
 else
-  s6_arch="x86_64"
+  s6_arch="amd64"
 fi
 
 case "$s6_arch" in
@@ -15,37 +15,45 @@ case "$s6_arch" in
     s6_arch="aarch64"
     ;;
   amd64)
-    s6_arch="x86_64"
+    s6_arch="amd64"
     ;;
 esac
-curl -sX GET "https://api.github.com/repos/just-containers/s6-overlay/releases/latest" | awk '/tag_name/{print $4;exit}' FS='[""]' >/etc/S6_RELEASE && \
-v1=`cat /etc/S6_RELEASE`
-v2=${v1//[v]/-}
 
-s6_url="https://github.com/just-containers/s6-overlay/releases/download/$v1/s6-overlay-noarch$v2.tar.xz"
+s6_url="https://github.com/just-containers/s6-overlay/releases/download/v2.2.0.3/s6-overlay-amd64.tar.gz"
 echo "Downloading from $s6_url" && \
-wget $s6_url -O /tmp/s6-overlay.tar.xz && \
-tar -C / -Jxpf /tmp/s6-overlay.tar.xz && \
-rm /tmp/s6-overlay.tar.xz && \
-echo "Installed s6-overlay $v1 (noarch)"
+wget $s6_url -O /tmp/s6-overlay.tar.gz && \
+tar xzf /tmp/s6-overlay.tar.gz -C / && \
+rm /tmp/s6-overlay.tar.gz && \
+echo "Installed s6-overlay v2.2.0.3"
 
-s6_url="https://github.com/just-containers/s6-overlay/releases/download/$v1/s6-overlay-$s6_arch$v2.tar.xz"
-echo "Downloading from $s6_url" && \
-wget $s6_url -O /tmp/s6-overlay.tar.xz && \
-tar -C / -Jxpf /tmp/s6-overlay.tar.xz && \
-rm /tmp/s6-overlay.tar.xz && \
-echo "Installed s6-overlay $v1 ($s6_arch)"
-
-s6_url="https://github.com/just-containers/s6-overlay/releases/download/$v1/s6-overlay-symlinks-noarch$v2.tar.xz"
-echo "Downloading from $s6_url" && \
-wget $s6_url -O /tmp/s6-overlay.tar.xz && \
-tar -C / -Jxpf /tmp/s6-overlay.tar.xz && \
-rm /tmp/s6-overlay.tar.xz && \
-echo "Installed s6-overlay $v1 (noarch)"
-
-s6_url="https://github.com/just-containers/s6-overlay/releases/download/$v1/s6-overlay-symlinks-arch$v2.tar.xz"
-echo "Downloading from $s6_url" && \
-wget $s6_url -O /tmp/s6-overlay.tar.xz && \
-tar -C / -Jxpf /tmp/s6-overlay.tar.xz && \
-rm /tmp/s6-overlay.tar.xz && \
-echo "Installed s6-overlay $v1 ($s6_arch)"
+#curl -sX GET "https://api.github.com/repos/just-containers/s6-overlay/releases/latest" | awk '/tag_name/{print $4;exit}' FS='[""]' >/etc/S6_RELEASE && \
+#v1=`cat /etc/S6_RELEASE`
+#v2=${v1//[v]/-}
+#
+#s6_url="https://github.com/just-containers/s6-overlay/releases/download/$v1/s6-overlay-noarch$v2.tar.xz"
+#echo "Downloading from $s6_url" && \
+#wget $s6_url -O /tmp/s6-overlay.tar.xz && \
+#tar -C / -Jxpf /tmp/s6-overlay.tar.xz && \
+#rm /tmp/s6-overlay.tar.xz && \
+#echo "Installed s6-overlay $v1 (noarch)"
+#
+#s6_url="https://github.com/just-containers/s6-overlay/releases/download/$v1/s6-overlay-$s6_arch$v2.tar.xz"
+#echo "Downloading from $s6_url" && \
+#wget $s6_url -O /tmp/s6-overlay.tar.xz && \
+#tar -C / -Jxpf /tmp/s6-overlay.tar.xz && \
+#rm /tmp/s6-overlay.tar.xz && \
+#echo "Installed s6-overlay $v1 ($s6_arch)"
+#
+#s6_url="https://github.com/just-containers/s6-overlay/releases/download/$v1/s6-overlay-symlinks-noarch$v2.tar.xz"
+#echo "Downloading from $s6_url" && \
+#wget $s6_url -O /tmp/s6-overlay.tar.xz && \
+#tar -C / -Jxpf /tmp/s6-overlay.tar.xz && \
+#rm /tmp/s6-overlay.tar.xz && \
+#echo "Installed s6-overlay $v1 (noarch)"
+#
+#s6_url="https://github.com/just-containers/s6-overlay/releases/download/$v1/s6-overlay-symlinks-arch$v2.tar.xz"
+#echo "Downloading from $s6_url" && \
+#wget $s6_url -O /tmp/s6-overlay.tar.xz && \
+#tar -C / -Jxpf /tmp/s6-overlay.tar.xz && \
+#rm /tmp/s6-overlay.tar.xz && \
+#echo "Installed s6-overlay $v1 ($s6_arch)"
