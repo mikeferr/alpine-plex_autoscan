e00851c7f494d5831c01cca1a99c2d4b5f63e86f
mikeferr <mikeferr82@gmail.com>
2022-02-22 06:35:22 -0500

    Update get_s6-overlay.sh

--- root/tmp/get_s6-overlay.sh
+++ root/tmp/get_s6-overlay.sh
@@ -21,6 +21,14 @@ esac
 curl -sX GET "https://api.github.com/repos/just-containers/s6-overlay/releases/latest" | awk '/tag_name/{print $4;exit}' FS='[""]' >/etc/S6_RELEASE && \
 v1=`cat /etc/S6_RELEASE`
 v2=${v1//[v]/-}
+
+s6_url="https://github.com/just-containers/s6-overlay/releases/download/$v1/s6-overlay-noarch$v2.tar.xz"
+echo "Downloading from $s6_url" && \
+wget $s6_url -O /tmp/s6-overlay.tar.xz && \
+tar -C / -Jxpf /tmp/s6-overlay.tar.xz && \
+rm /tmp/s6-overlay.tar.xz && \
+echo "Installed s6-overlay $v1 (noarch)"
+
 s6_url="https://github.com/just-containers/s6-overlay/releases/download/$v1/s6-overlay-$s6_arch$v2.tar.xz"
 echo "Downloading from $s6_url" && \
 wget $s6_url -O /tmp/s6-overlay.tar.xz && \
